064e1868405b8bc6c86cd4017a66b5fc6cc31ba9
Namrata Mahanta <namratamahantaofficial@gmail.com>
2022-07-26 07:02:40 +0530

    Initial commit

---
+++ src/main/java/com/cg/book/app/service/CategoryService.java
@@ -0,0 +1,18 @@
+package com.cg.book.app.service;
+
+import java.util.List;
+
+
+import com.cg.book.app.model.Category;
+
+
+public interface CategoryService {
+
+	Category addCategory(Category category);
+	
+	List<Category> getAllCategory();
+	
+
+	Category getcategoryById(int id);
+
+}
